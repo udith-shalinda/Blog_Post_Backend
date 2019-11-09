02711cfb0a28d6f82186b7883713a77d65a8e47d
Udith <udithshalinda2@gmail.com>
2019-11-09 12:45:47 +0530

    update

--- src/main/java/com/udithshalinda/demo/DemoApplication.java
+++ src/main/java/com/udithshalinda/demo/DemoApplication.java
@@ -1,9 +1,5 @@
 package com.udithshalinda.demo;
 
-import com.udithshalinda.demo.customer.Customer;
-import com.udithshalinda.demo.customer.CustomerRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-	@Autowired
-	private CustomerRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
